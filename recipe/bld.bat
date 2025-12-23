39a5668d6ededc0627ba7370b0a4de435f901877
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 21:55:45 -0600

    update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,6 @@
 :: Export symbols in RcppExports.cpp
 sed -i -e "s/void R_init_quanteda_textstats/__declspec(dllexport) void R_init_quanteda_textstats/" src/RcppExports.cpp
+IF %ERRORLEVEL% NEQ 0 exit 1
 :: Remove part of PKG_LIBS that tries to link non-existent libraries.
 sed -i -e "s/$(LAPACK_LIBS) $(BLAS_LIBS)//" src/Makevars.win
 IF %ERRORLEVEL% NEQ 0 exit 1
