4d2e0eda287f196850d202832d81eef5734bc521
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-22 21:48:29 -0600

    use tbb-devel

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,8 @@
+:: Export symbols in RcppExports.cpp
+sed -i -e "s/void R_init_quanteda_textstats/__declspec(dllexport) void R_init_quanteda_textstats/" src/RcppExports.cpp
+:: Remove part of PKG_LIBS that tries to link non-existent libraries.
+sed -i -e "s/$(LAPACK_LIBS) $(BLAS_LIBS)//" src/Makevars.win
+IF %ERRORLEVEL% NEQ 0 exit 1
+
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
